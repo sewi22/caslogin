fcb87ea6151a6dcc40f1189c20a0b5d3db0739fb
Sebastian Winterling <sebastian.winterling@iem.thm.de>
2015-04-01 23:26:59 +0200

    1.0.51

--- js/navigation.js
+++ js/navigation.js
@@ -12,18 +12,18 @@
     $.mobile.document.on('touchend click', '#caslogoutbutton', function(e){
         e.preventDefault();
         console.log("Logout from CAS");
-        //var url = "https://cas.thm.de/cas/logout"
-        var url = "http://google.com";
+        var url = "https://cas.thm.de/cas/logout";
+        //var url = "http://google.com";
         var homeurl = encodeURIComponent("http://phylab.org/app/");        
         //window.location = url+"?url="+homeurl;
         //window.plugins.ChildBrowser.showWebPage(url,{showLocationBar: false, showAddress: false, showNavigationBar: false});
         //window.plugins.ChildBrowser.openExternal(url, true);
         //window.plugins.ChildBrowser.onLocationChange = function (url) {
-        alert("test");
+        //alert("test");
         iab = window.open(url,'_blank','location=yes,hidden=no');
-        setTimeout(function() {
-             iab.close();
-         }, 2500);
+        //setTimeout(function() {
+        //     iab.close();
+        // }, 2500);
         //iab.addEventListener('loadstart', iabLoadStart);
         iab.addEventListener('loadstop', iabLoadStop);
         iab.addEventListener('loaderror', iabLoadError);
